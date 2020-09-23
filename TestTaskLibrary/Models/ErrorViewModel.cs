ae99531a4234ec40992648400caa5204d02c9dc8
Max <maxima-2000@mail.ru>
2020-09-23 19:18:14 +0300

    Add project files.

---
+++ TestTaskLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestTaskLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
