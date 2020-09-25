a568a9b20f3c3df4e68f609d15b033543c5b0922
Max <maxima-2000@mail.ru>
2020-09-25 19:15:38 +0300

    Вернул игнорированный файл

---
+++ TestTaskLibrary.Domain.Core/Status.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TestTaskLibrary.Domain.Core
+{
+    public enum Status
+    {
+        [Display(Name = "Доступна")]
+        Free,
+
+        [Display(Name = "Забронирована")]
+        Booked,
+
+        [Display(Name = "Выдана")]
+        Issued
+    }
+}
