17cf44e172f68009f05f126e3a1bd41f40934bff
Max <maxima-2000@mail.ru>
2020-10-06 22:30:33 +0300

    Сделан Generic репозиторий

---
+++ TestTaskLibrary.Domain.Interfaces/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskLibrary.Domain.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll { get;}
+        Task Add(TEntity item);
+        Task AddRange(IEnumerable<TEntity> items);
+        Task Remove(TEntity item);
+        Task Update(TEntity item);
+    }
+}
---
+++ TestTaskLibrary.Infrastructure.Data/Repositories/EFGenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTaskLibrary.Domain.Application.Interfaces.Repositories;
+
+namespace TestTaskLibrary.Infrastructure.Data.Repositories
+{
+    public class EFGenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly LibraryContext context;
+        private readonly DbSet<TEntity> dbSet;
+
+        public EFGenericRepository(LibraryContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<TEntity>();
+            GetAll = dbSet;
+        }
+
+        public IQueryable<TEntity> GetAll { get; }
+
+        public async Task Add(TEntity item)
+        {
+            dbSet.Add(item);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task AddRange(IEnumerable<TEntity> items)
+        {
+            dbSet.AddRange(items);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Remove(TEntity item)
+        {
+            dbSet.Remove(item);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(TEntity item)
+        {
+            dbSet.Update(item);
+            await context.SaveChangesAsync();
+        }
+    }
+}
